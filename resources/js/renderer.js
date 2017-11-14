5974bafcce3d5ac643995de2c9c985bf2add072c
Luke Klinker <lklinker1@gmail.com>
2017-11-13 20:49:39 -0600

    monkey around with the loaded webvbview pages, after they are loaded, to make them look a bit nicer in the compact views

--- resources/js/renderer.js
+++ resources/js/renderer.js
@@ -1,21 +1,29 @@
-// var webview = document.getElementById('messenger')
-//
-// window.addEventListener("focus", function() {
-//     webview.focus()
-// });
-//
-// webview.addEventListener("dom-ready", function() {
-//     webview.focus()
-// });
-//
-// webview.addEventListener('ipc-message', function(event) {
-//
-// });
-//
-// webview.addEventListener('new-window', function(event) {
-//   try {
-//     require('electron').shell.openExternal(event.url)
-//   } catch (error) {
-//     console.log("Ignoring " + event.url + " due to " + error.message)
-//   }
-// })
+var talon = document.getElementById('talon-reviews')
+var pulse = document.getElementById('pulse-reviews')
+var purchases = document.getElementById('purchases')
+
+talon.addEventListener("dom-ready", function() {
+    setTimeout(function() {
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-J-e')[0].style.display = "none";`)
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-J-f')[0].style.display = "none";`)
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-E-h MKC43RB-j-C MKC43RB-E-i MKC43RB-Jm-a')[0].style.display = "none";`)
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-p-c')[0].style.display = "none";`)
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-k')[0].innerHTML = "Talon for Twitter";`)
+    }, 4000)
+})
+
+pulse.addEventListener("dom-ready", function() {
+    setTimeout(function() {
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-J-e')[0].style.display = "none";`)
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-J-f')[0].style.display = "none";`)
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-E-h MKC43RB-j-C MKC43RB-E-i MKC43RB-Jm-a')[0].style.display = "none";`)
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-p-c')[0].style.display = "none";`)
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-k')[0].innerHTML = "Pulse SMS";`)
+    }, 4000)
+})
+
+purchases.addEventListener("dom-ready", function() {
+    setTimeout(function() {
+      purchases.executeJavaScript(`document.getElementsByClassName('MKC43RB-f-m')[0].click();`)
+    }, 3000)
+})
