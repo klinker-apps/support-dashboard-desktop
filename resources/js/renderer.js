b930669c41554795bda1b349726dd41e1c2b1386
Luke Klinker <lklinker1@gmail.com>
2017-11-14 10:55:26 -0600

    add a twitter search page

--- resources/js/renderer.js
+++ resources/js/renderer.js
@@ -2,6 +2,35 @@ var talon = document.getElementById('talon-reviews')
 var pulse = document.getElementById('pulse-reviews')
 var purchases = document.getElementById('purchases')
 var admin = document.getElementById('pulse-admin')
+var twitter = document.getElementById('twitter-search')
+
+talon.addEventListener("focus", function() {
+    talon.style.height = "52%"
+    pulse.style.height = "16%"
+    admin.style.height = "16%"
+    twitter.style.height = "16%"
+})
+
+pulse.addEventListener("focus", function() {
+    pulse.style.height = "52%"
+    talon.style.height = "16%"
+    admin.style.height = "16%"
+    twitter.style.height = "16%"
+})
+
+admin.addEventListener("focus", function() {
+    admin.style.height = "52%"
+    talon.style.height = "16%"
+    pulse.style.height = "16%"
+    twitter.style.height = "16%"
+})
+
+twitter.addEventListener("focus", function() {
+    twitter.style.height = "52%"
+    talon.style.height = "16%"
+    pulse.style.height = "16%"
+    admin.style.height = "16%"
+})
 
 talon.addEventListener("dom-ready", function() {
     setTimeout(function() {
@@ -40,9 +69,15 @@ admin.addEventListener("dom-ready", function() {
     }, 1500)
 })
 
-function tryCatch(code) {
-  return "try { " + code + " } catch (err) { }"
-}
+twitter.addEventListener("dom-ready", function() {
+    setTimeout(function() {
+      twitter.executeJavaScript(tryCatch(`document.getElementsByClassName('Grid-cell u-size1of3 u-lg-size1of4')[0].style.display = "none";`))
+      twitter.executeJavaScript(tryCatch(`document.getElementsByClassName('SearchNavigation')[0].style.display = "none";`))
+      twitter.executeJavaScript(tryCatch(`document.getElementsByClassName('global-nav')[0].style.display = "none";`))
+      twitter.executeJavaScript(tryCatch(`document.getElementById('page-container').style.paddingTop = "0px";`))
+      twitter.focus()
+    }, 1000)
+})
 
 setTimeout(reloadPurchases, 1 * 60 * 1000)
 setTimeout(reloadReviews, 3 * 60 * 1000)
@@ -54,7 +89,16 @@ function reloadReviews() {
     setTimeout(reloadReviews, 3 * 60 * 1000)
 }
 
+function reloadTwitter() {
+    twitter.reload()
+    setTimeout(reloadTwitter, 5 * 60 * 1000)
+}
+
 function reloadPurchases() {
     purchases.loadURL("https://play.google.com/apps/publish/?account=6337185423976596164#OrderManagementPlace")
     setTimeout(reloadPurchases, 1 * 60 * 1000)
+}
+
+function tryCatch(code) {
+  return "try { " + code + " } catch (err) { }"
 }
