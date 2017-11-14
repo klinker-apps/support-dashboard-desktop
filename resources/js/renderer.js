16769003023a4ce048b148443cdca4d9c46832e7
Luke Klinker <lklinker1@gmail.com>
2017-11-14 09:02:12 -0600

    hide more unnecessary elements on the dashboard pages

--- resources/js/renderer.js
+++ resources/js/renderer.js
@@ -1,6 +1,7 @@
 var talon = document.getElementById('talon-reviews')
 var pulse = document.getElementById('pulse-reviews')
 var purchases = document.getElementById('purchases')
+var admin = document.getElementById('pulse-admin')
 
 talon.addEventListener("dom-ready", function() {
     setTimeout(function() {
@@ -8,8 +9,9 @@ talon.addEventListener("dom-ready", function() {
       talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-J-f')[0].style.display = "none";`)
       talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-E-h MKC43RB-j-C MKC43RB-E-i MKC43RB-Jm-a')[0].style.display = "none";`)
       talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-p-c')[0].style.display = "none";`)
-      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-k')[0].innerHTML = "Talon for Twitter";`)
-    }, 4000)
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-h MKC43RB-s-b MKC43RB-j-u')[0].style.display = "none";`)
+      talon.executeJavaScript(`document.getElementsByClassName('MKC43RB-j-u')[2].style.display = "none";`)
+    }, 5000)
 })
 
 pulse.addEventListener("dom-ready", function() {
@@ -18,12 +20,22 @@ pulse.addEventListener("dom-ready", function() {
       pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-J-f')[0].style.display = "none";`)
       pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-E-h MKC43RB-j-C MKC43RB-E-i MKC43RB-Jm-a')[0].style.display = "none";`)
       pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-p-c')[0].style.display = "none";`)
-      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-k')[0].innerHTML = "Pulse SMS";`)
-    }, 4000)
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-h MKC43RB-s-b MKC43RB-j-u')[0].style.display = "none";`)
+      pulse.executeJavaScript(`document.getElementsByClassName('MKC43RB-j-u')[2].style.display = "none";`)
+    }, 5000)
 })
 
 purchases.addEventListener("dom-ready", function() {
     setTimeout(function() {
       purchases.executeJavaScript(`document.getElementsByClassName('MKC43RB-f-m')[0].click();`)
+      purchases.executeJavaScript(`document.getElementsByClassName('MKC43RB-G-h MKC43RB-s-b MKC43RB-j-u')[0].style.display = "none";`)
+      purchases.executeJavaScript(`document.getElementsByClassName('MKC43RB-j-u')[2].style.display = "none";`)
     }, 3000)
+})
+
+admin.addEventListener("dom-ready", function() {
+    setTimeout(function() {
+      admin.executeJavaScript(`document.getElementsByClassName('mdl-layout__header is-casting-shadow')[0].style.display = "none";`)
+      admin.executeJavaScript(`document.getElementById('admin-list').style.paddingTop = "0px";`)
+    }, 1500)
 })
