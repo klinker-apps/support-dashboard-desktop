9d8da22a26ace0946fa8fe4f4bc56e1f0b00a27b
Luke Klinker <lklinker1@gmail.com>
2017-12-26 09:00:36 -0600

    add the 'view' menu back

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -32,7 +32,34 @@
                   createWindow("https://messenger.klinkerapps.com/overview")
               }
           }]
+      }, {
+      label: 'View',
+      submenu: [{
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click(item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload()
+        }
+      }, {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+        }
+      }, {
+        type: 'separator'
+      }, {
+        role: 'resetzoom'
+      }, {
+        role: 'zoomin'
+      }, {
+        role: 'zoomout'
+      }, {
+        type: 'separator'
+      }, {
+        role: 'togglefullscreen'
       }]
+    },]
 
 
       if (process.platform === 'darwin') {
