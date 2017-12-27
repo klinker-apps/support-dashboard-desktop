c243a70031bfa42f5f2fb4b4d0880ffe17e1f52b
Luke Klinker <lklinker1@gmail.com>
2017-12-26 20:22:44 -0600

    add the edit menu back

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -33,6 +33,27 @@
               }
           }]
       }, {
+      label: 'Edit',
+      submenu: [{
+        role: 'undo'
+      }, {
+        role: 'redo'
+      }, {
+        type: 'separator'
+      }, {
+        role: 'cut'
+      }, {
+        role: 'copy'
+      }, {
+        role: 'paste'
+      }, {
+        role: 'pasteandmatchstyle'
+      }, {
+        role: 'delete'
+      }, {
+        role: 'selectall'
+      }]
+    }, {
       label: 'View',
       submenu: [{
         label: 'Reload',
