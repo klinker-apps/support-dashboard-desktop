ce37fb2003334382ba3a74d5434d5a94f0429767
Luke Klinker <lklinker1@gmail.com>
2018-03-28 08:18:16 -0500

    update play store prefix

--- resources/js/renderer.js
+++ resources/js/renderer.js
@@ -1,7 +1,7 @@
 var firstTime = true
 var playStorePrefixes = ["CNWV0SC", "NASRHNB","OOQU5YB", "IF2W6TD", "K36O22",
               "H6VDBHC", "JGIQJE", "MNKEEGB", "FQXSPUC", "APEYB2C", "DSO2KAC",
-              "HCXDKGD", "JIM1JT", "CRJ1NGC", "G035G"]
+              "HCXDKGD", "JIM1JT", "CRJ1NGC", "G035G", "KF51A6D"]
 
 var talon = document.getElementById('talon-reviews')
 var pulse = document.getElementById('pulse-reviews')
