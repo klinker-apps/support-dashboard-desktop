fbcca0fc7d3f194cdca25db75c62b84c0a0c7367
Luke Klinker <lklinker1@gmail.com>
2018-10-06 21:43:58 -0500

    refactor and fix an issue for Windows/Linux

--- main.js
+++ main.js
@@ -11,10 +11,6 @@ const menu = require('./resources/js/menu.js')
 
 app.requestSingleInstanceLock()
 app.on('second-instance', () => {
-  if (windowProvider == null) {
-    initialize()
-  }
-
   windowProvider.getWindow().show()
 })
 
@@ -34,46 +30,15 @@ app.on('before-quit', () => {
 
 function createWindow() {
   if (windowProvider.getWindow() == null) {
-    let mainWindowState = windowStateKeeper({
-      defaultWidth: 1000,
-      defaultHeight: 750
-    })
-
-    var mainWindow = new BrowserWindow({
-      'x': mainWindowState.x,
-      'y': mainWindowState.y,
-      'width': mainWindowState.width,
-      'height': mainWindowState.height,
-      'titleBarStyle': 'hidden'
-    })
-
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-
-    mainWindow.on('close', function(event) {
-      event.preventDefault();
-      windowProvider.getWindow().hide();
-    })
-
-    mainWindow.on('closed', function(event) {
-      windowProvider.setWindow(null)
-    })
-
-    mainWindow.setMenuBarVisibility(false)
-    mainWindow.setAutoHideMenuBar(true)
-
-    windowProvider.setWindow(mainWindow)
-    mainWindowState.manage(mainWindow)
-    menu.buildMenu(mainWindow)
+    let window = windowProvider.createMainWindow()
+    menu.buildMenu(window)
   } else {
     if (process.platform === 'darwin') {
       app.dock.show()
     }
 
-    windowProvider.getWindow().show()
-    menu.buildMenu(windowProvider.getWindow())
+    let window = windowProvider.getWindow()
+    window.show()
+    menu.buildMenu(window)
   }
 }
--- resources/js/window-provider.js
+++ resources/js/window-provider.js
@@ -1,6 +1,58 @@
 (function() {
+
+    const { BrowserWindow, app } = require('electron')
+
+    const windowStateKeeper = require('electron-window-state')
+    const path = require('path')
+    const url = require('url')
+
     let mainWindow = null
-    
+
+    var createMainWindow = () => {
+      let mainWindowState = windowStateKeeper({
+        defaultWidth: 1000,
+        defaultHeight: 750
+      })
+
+      let window = new BrowserWindow({
+        'x': mainWindowState.x,
+        'y': mainWindowState.y,
+        'width': mainWindowState.width,
+        'height': mainWindowState.height,
+        'titleBarStyle': 'hidden'
+      })
+
+      window.loadURL(url.format({
+        pathname: path.join(__dirname, '../../index.html'),
+        protocol: 'file:',
+        slashes: true
+      }))
+
+      window.on('close', function(event) {
+        event.preventDefault()
+
+        if (process.platform === 'darwin') {
+          window.hide()
+        } else {
+          // there is a super weird bug where Electron opens a blank window, when a hidden window is restored.
+          // I don't know why.. Happens on Windows and Linux.
+          window.minimize()
+        }
+      })
+
+      window.on('closed', function(event) {
+        event.preventDefault()
+      })
+
+      window.setMenuBarVisibility(false)
+      window.setAutoHideMenuBar(true)
+
+      setWindow(window)
+      mainWindowState.manage(window)
+
+      return window
+    }
+
     var setWindow = function(w) {
         mainWindow = w
     }
@@ -9,6 +61,7 @@
         return mainWindow
     }
 
+    module.exports.createMainWindow = createMainWindow;
     module.exports.getWindow = getWindow;
     module.exports.setWindow = setWindow;
 }());
