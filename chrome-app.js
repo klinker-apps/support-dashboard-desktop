466971a4679717be9921ebfa716f790645f4fe35
Luke Klinker <lklinker1@gmail.com>
2019-12-07 06:17:52 -0600

    add the ability to use the support dashboard as an unpacked chrome app

---
+++ chrome-app.js
@@ -0,0 +1,59 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+
+var screenWidth = screen.availWidth;
+var screenHeight = screen.availHeight;
+var width = 1100;
+var height = 700;
+
+var windowOptions = {
+  id: "mainWindowID",
+  bounds: {
+    width: width,
+    height: height,
+    left: Math.round((screenWidth - width) / 2),
+    top: Math.round((screenHeight - height) / 2)
+  },
+  frame: {
+    type: "chrome"
+  }
+};
+
+var windows = {};
+var lastNotificationTime = new Date().getTime();
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  var win = window;
+
+  var mainWindow = chrome.app.window.create(
+    "index.html",
+    windowOptions,
+    function(window) {
+      // window.contentWindow.onload = function() {
+      //   // Retrieve the webview element
+      //   var webview = window.contentWindow.document.querySelector("#calendar");
+      //   webview.setUserAgentOverride(navigator.userAgent + " chrome-app");
+
+      //   webview.addEventListener("newwindow", function(e) {
+      //     // open the link in a new chrome tab
+      //     e.preventDefault();
+      //     if (e.targetUrl.indexOf("google") === -1) {
+      //       win.open(e.targetUrl);
+      //     } else {
+      //       webview.src = e.targetUrl;
+      //     }
+      //   });
+
+      //   webview.addEventListener("dialog", function(e) {
+      //     // automatically accept the dialog
+      //     e.preventDefault();
+      //     e.dialog.ok("accepted");
+      //   });
+      // };
+    }
+  );
+});
--- loader.js
+++ loader.js
@@ -1,43 +1,65 @@
-const fs = require('fs')
-const path = require('path')
-const renderer = require('./renderer.js')
+// const fs = require('fs')
+// const path = require('path')
+// const renderer = require('./renderer.js')
 
-var current = null
+var current = null;
 
 var openPage = (pageName = "gmail") => {
   if (pageName == current) {
-    return
+    return;
   }
 
-  current = pageName
+  current = pageName;
 
-  fs.readFile(path.join(__dirname, 'pages/' + pageName + '.html'), (err, data) => {
-    if (err) {
-      console.log(err)
-      return
-    }
+  // document.getElementById('content').innerHTML = '<object type="text/html" data="pages/' + pageName + '.html"></object>'
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "pages/" + pageName + ".html", true);
+  xhr.onreadystatechange = function() {
+    document.getElementById("content").innerHTML = this.responseText;
+  };
+  xhr.send();
+  // fs.readFile(path.join(__dirname, 'pages/' + pageName + '.html'), (err, data) => {
+  //   if (err) {
+  //     console.log(err)
+  //     return
+  //   }
 
-    document.getElementById('content').innerHTML = data
-    renderer.initPage()
-  })
-}
+  //   document.getElementById('content').innerHTML = data
+  //   // renderer.initPage()
+  // })
+};
 
 // default to email page.
-openPage("gmail")
+openPage("gmail");
 
 // this defines the icons on the left and their order
-let navString = ""
+let navString = "";
 let navLinks = [
-  'gmail', 'slack', 'pulse', 'talon', 'purchase', 'github', 'twitter', 'cloud-consoles', 'jenkins'
-]
+  "gmail",
+  "slack",
+  "pulse",
+  "talon",
+  "purchase",
+  "github",
+  "twitter",
+  "cloud-consoles",
+  "jenkins"
+];
 
 for (let i = 0; i < navLinks.length; i++) {
-  navString += '<img id="nav-' + navLinks[i] + '" class="nav-item" src="resources/images/' + navLinks[i] + '.png">'
+  navString +=
+    '<img id="nav-' +
+    navLinks[i] +
+    '" class="nav-item" src="resources/images/' +
+    navLinks[i] +
+    '.png">';
 }
 
-document.getElementById('sidebar').innerHTML = navString
+document.getElementById("sidebar").innerHTML = navString;
 
 for (let i = 0; i < navLinks.length; i++) {
-  let element = document.getElementById('nav-' + navLinks[i])
-  element.addEventListener("click", () => { openPage(navLinks[i]) })
+  let element = document.getElementById("nav-" + navLinks[i]);
+  element.addEventListener("click", () => {
+    openPage(navLinks[i]);
+  });
 }
